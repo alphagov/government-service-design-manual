fc727272d0336296a4b6aa959e613b7134258e13
Alex Torrance <hello@alextorrance.co.uk>
2013-02-20 14:56:38 +0000

    .gitignore now working

--- _site/assets/javascripts/design-patterns/emphasised_form_controls.js
+++
@@ -1,8 +0,0 @@
-// Emphasised checkbox and radio button label styles
-    
-var $emphasised = $('.emphasised input');
-$emphasised.filter(':checked').parent().addClass('checked');
-$emphasised.change(function() {
-    $emphasised.parent().removeClass('checked');
-    $emphasised.filter(':checked').parent().addClass('checked');
-});
