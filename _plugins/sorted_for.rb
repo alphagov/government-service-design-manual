0832c6f8f78cdd695c51d6a229742e1eaeac4db2
Alex Torrance <hello@alextorrance.co.uk>
2013-02-20 13:36:26 +0000

    Add sorted_for plugin
    This allows us to sort and iterate over arrays

---
+++ _plugins/sorted_for.rb
@@ -0,0 +1,63 @@
+module Jekyll
+  module SortedForImpl
+    def render(context)
+      sorted_collection = collection_to_sort context
+      return if sorted_collection.empty?
+      sort_attr = @attributes['sort_by']
+      case_sensitive = @attributes['case_sensitive'] == 'true'
+      i = sorted_collection.first
+ 
+      if sort_attr != nil
+        if i.to_liquid[sort_attr].instance_of? String and not case_sensitive
+          sorted_collection.sort_by! { |i| i.to_liquid[sort_attr].downcase }
+        else
+          sorted_collection.sort_by! { |i| i.to_liquid[sort_attr] }
+        end
+      else
+        if i.instance_of? String and not case_sensitive
+          sorted_collection.sort_by! { |i| i.downcase }
+        else
+          sorted_collection.sort!
+        end
+      end
+ 
+      original_name = @collection_name
+      result = nil
+      context.stack do
+        sorted_collection_name = "#{@collection_name}_sorted".sub('.', '_')
+        context[sorted_collection_name] = sorted_collection
+        @collection_name = sorted_collection_name
+        result = super
+        @collection_name = original_name
+      end
+      result
+    end
+  end
+ 
+  class SortedForTag < Liquid::For
+    include SortedForImpl
+ 
+    def collection_to_sort(context)
+      return context[@collection_name].dup
+    end
+ 
+    def end_tag
+      'endsorted_for'
+    end
+  end
+ 
+  class SortedKeysForTag < Liquid::For
+    include SortedForImpl
+ 
+    def collection_to_sort(context)
+      return context[@collection_name].keys
+    end
+ 
+    def end_tag
+      'endsorted_keys_for'
+    end
+  end
+end
+ 
+Liquid::Template.register_tag('sorted_for', Jekyll::SortedForTag)
+Liquid::Template.register_tag('sorted_keys_for', Jekyll::SortedKeysForTag)
