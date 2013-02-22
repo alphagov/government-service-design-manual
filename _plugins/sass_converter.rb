95778d09079860d1746f2c478146bd7a2d61fb00
Alex Torrance <hello@alextorrance.co.uk>
2013-02-22 16:13:14 +0000

    Split SCSS into views and helpers
    An attempt to rationalise and organise the CSS structure so that helpers and views are separate, making editing and maintenance easier

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -21,7 +21,7 @@ module Jekyll
         t = Time.now.strftime("%Y-%m-%d %H:%M:%S");
         puts "[#{t}] Performing Sass Conversion."
         gem_dir = Bundler.load.specs.find{|s| s.name == 'govuk_frontend_toolkit' }.full_gem_path
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./_includes/stylesheets/", gem_dir + "/app/assets/stylesheets/"], :line_numbers => true)
+        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./_includes/stylesheets/", gem_dir + "/app/assets/stylesheets/", "./assets/stylesheets/"], :line_numbers => true)
         engine.render
       rescue StandardError => e
         puts "[#{t}] !!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
