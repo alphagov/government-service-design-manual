45da513e0ad3785d931627eeef7af70e6543e0da
Alex Torrance <hello@alextorrance.co.uk>
2013-03-01 10:59:47 +0000

    Table contents now recursively iterates the tree
    Now shows sub-sections.

--- _plugins/toc_list.rb
+++ _plugins/toc_list.rb
@@ -2,17 +2,35 @@ module Jekyll
   module TableOfContentsFilter
     def toc_list(document)
       toc = []
+      html = ''
       content = Kramdown::Document.new(document)
       toc_tree = Kramdown::Converter::Toc.convert(content.root).first
+      html << "<ul>\n"
       toc_tree.children.each do |heading|
-        p heading
-        html_id = heading.attr[:id]
-        text = heading.value.options[:raw_text]
-        toc << "<li class=\"level-#{html_id}\"><a href=\"\##{html_id}\">#{text}</a></li>"
+        html << show_tree(heading)
       end
-      "<ul>\n" + toc.join("\n") + "\n</ul>"
+      html << "</ul>\n"
     end
   end
+end
+
+def show_tree(my_item)
+  html = ''
+  html_id = my_item.attr[:id]
+  text = my_item.value.options[:raw_text]
+  html << "<li class=\"level-#{html_id}\"><a href=\"\##{html_id}\">#{text}</a>\n"
+
+  if my_item.children.any?
+    html << "<ul>\n"
+    my_item.children.each do |my_child|
+      html << show_tree(my_child)
+    end
+    html << "</ul>\n"
+  end
+
+  html << "</li>\n"
+
+  return html
 end
 
 Liquid::Template.register_filter(Jekyll::TableOfContentsFilter)
