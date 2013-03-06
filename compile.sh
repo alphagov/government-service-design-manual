b8fd0de1742113d1e346165fd489bf71f6889334
James Stewart <jys@ketlai.co.uk>
2013-03-06 11:33:07 +0000

    Add script to compile content to HTML and copy to design-principles

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIRECTORY="../design-principles/public"
+GUIDANCE_PATH="$DIRECTORY/guidance"
+
+if [ ! -d "$DIRECTORY" ]; then
+  echo "Couldn't find design principles app in $DIRECTORY"
+fi
+
+if [ ! -d $GUIDANCE_PATH ]; then
+  echo "Creating guidance folder"
+  mkdir $GUIDANCE_PATH
+else
+  echo "Emptying existing guidance folder"
+  rm -rf $GUIDANCE_PATH
+fi
+
+bundle exec jekyll $GUIDANCE_PATH
+find $GUIDANCE_PATH -name "*.html" -exec sed -i -e 's#href="/#href="/service-manual/#g' {} \;
+
+echo "*****"
+echo "You should now use git to push the changes to the design principles app."
+echo "And then schedule a deployment for it."
